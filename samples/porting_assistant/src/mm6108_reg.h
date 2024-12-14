a198a42a5d2e5a8c111ee7557c3e8d389a7fa403
Arien Judge <arien.judge@morsemicro.com>
2024-12-15 03:20:12 +1100

    samples: porting_assistant: add porting assitant application
    The Morse Micro porting assistant application is a cruical part of
    any hardware integration exercise. This application runs through a
    number of basic tests to ensure the MM6108 can correctly initialise
    and various IRQ pins are correctly configured.

---
+++ samples/porting_assistant/src/mm6108_reg.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2024 Morse Micro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+
+/** Address where the chip ID is stored on the MM6108 */
+#define MM6108_REG_CHIP_ID     0x10054d20
+
+#define MM6108_REG_OUT_EN      0x10012008
+#define MM6108_REG_OUT_VAL     0x1001200c
