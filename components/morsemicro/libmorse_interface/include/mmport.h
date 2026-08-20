1086aa10d8b54c952441438c0eb3fc1d869b7cb4
Arien Judge <arien.judge@morsemicro.com>
2026-08-20 16:03:56 +1000

    libmorse_interface: remove command line MMOSAL_ASSERT* definitions
    These definitions on the command line were throwing warnings during compilation.
    We carry an mmport header which, if implemented correctly, means there is no reason
    for MMOSAL_ASSERT macros to not pick up the default definitions from the mm-iot-sdk.
    
    Tracked-by: OSP-967
    Signed-off-by: Arien Judge <arien.judge@morsemicro.com>

--- components/morsemicro/libmorse_interface/include/mmport.h
+++ components/morsemicro/libmorse_interface/include/mmport.h
@@ -7,6 +7,6 @@
 #pragma once
 
 #define MMPORT_BREAKPOINT()
-#define MMPORT_GET_LR()
-#define MMPORT_GET_PC(_a)
+#define MMPORT_GET_LR()     0
+#define MMPORT_GET_PC(_a)   0
 #define MMPORT_MEM_SYNC()   __sync_synchronize()
